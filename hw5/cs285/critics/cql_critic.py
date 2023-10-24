d5e453e1504a611449e6787ae594ac41b94a36af
Qingyang Song <ollie@D-10-18-101-218.dhcp4.washington.edu>
2023-10-24 15:38:42 -0700

    updating hw5

--- hw5/cs285/critics/cql_critic.py
+++ hw5/cs285/critics/cql_critic.py
@@ -44,7 +44,18 @@ class CQLCritic(BaseCritic):
 
     def dqn_loss(self, ob_no, ac_na, next_ob_no, reward_n, terminal_n):
         """ Implement DQN Loss """
+        qa_t_values = self.q_net(ob_no)
+        q_t_values = torch.gather(qa_t_values, 1, ac_na.unsqueeze(1)).squeeze(1)
+        qa_tn_values = self.q_net_target(next_ob_no)
 
+        if self.double_q:
+            next_actions = torch.argmax(self.q_net(next_ob_no), dim=1)
+            q_tn = torch.gather(qa_tn_values, 1, next_actions.unsqueeze(1)).squeeze(1)
+        else:
+            q_tn, _ = torch.max(qa_tn_values, dim=1)
+
+        target = reward_n + self.gamma * q_tn * (1-terminal_n)
+        loss = self.loss(q_t_values, target.detach())
         return loss, qa_t_values, q_t_values
 
 
@@ -78,15 +89,15 @@ class CQLCritic(BaseCritic):
         # CQL Implementation
         # TODO: Implement CQL as described in the pdf and paper
         # Hint: After calculating cql_loss, augment the loss appropriately
-        q_t_logsumexp = None
-        cql_loss = None
+        q_t_logsumexp = torch.logsumexp(qa_t_values, dim=1)
+        cql_loss = torch.mean(q_t_logsumexp - q_t_values, dim=0)
 
         info = {'Training Loss': ptu.to_numpy(loss)}
 
         # TODO: Uncomment these lines after implementing CQL
-        # info['CQL Loss'] = ptu.to_numpy(cql_loss)
-        # info['Data q-values'] = ptu.to_numpy(q_t_values).mean()
-        # info['OOD q-values'] = ptu.to_numpy(q_t_logsumexp).mean()
+        info['CQL Loss'] = ptu.to_numpy(cql_loss)
+        info['Data q-values'] = ptu.to_numpy(q_t_values).mean()
+        info['OOD q-values'] = ptu.to_numpy(q_t_logsumexp).mean()
         
         self.learning_rate_scheduler.step()
 
